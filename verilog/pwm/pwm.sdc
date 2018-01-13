015463c2b957723db1406d5c589a90ad77e2df7f
Glenn K. Lockwood <glennklockwood@gmail.com>
2018-01-13 11:20:39 -0800

    use four dip switches to control duty cycle; decrease pwm drive frequency

--- verilog/pwm/pwm.sdc
+++ verilog/pwm/pwm.sdc
@@ -1,3 +1,3 @@
 create_clock -name clk -period "50MHz" [get_ports clock]
 
-set_false_path -from * -to [get_ports LED]
+set_false_path -from * -to [get_ports {led dips[*]}]
--- verilog/pwm/pwm.v
+++ verilog/pwm/pwm.v
@@ -1,19 +1,36 @@
 /*
  * demonstrate PWM with Verilog
  */
-
-module pwm(clock, LED);
+module pwm(clock, dips, led);
     input wire clock;
-    output wire LED;
-    reg [31:0] count;
+    input wire dips[3:0];
+    output wire led;
+    reg [15:0] count;
+    reg [15:0] duty;
 
     initial begin
-        count <= 32'b0;
+        count <= 16'b0;
+        duty <= 16'b0;
     end
 
+    /* 
+     * 50 MHz clock = 20 ns per clock 
+     * 8-bit register = 5.12 us between overflows (195.3 kHz)
+     * 16-bit register = 1.31 ms between overflows (762.9 Hz)
+     */
     always @(posedge clock) begin
-        count <= count + 1'b1;
+        count = count + 1'b1;
+        if (count > duty)
+            led <= 1'b1;
+        else
+            led <= 1'b0;
     end
 
-    assign LED = count[24];
+    /*
+     * four dip switches control the most significant bits of duty
+     */
+    assign duty[15] = dips[3];
+    assign duty[14] = dips[2];
+    assign duty[13] = dips[1];
+    assign duty[12] = dips[0];
 endmodule
