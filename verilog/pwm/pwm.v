51113ce53c527f604a671445133fa2d445b8d447
Glenn K. Lockwood <glennklockwood@gmail.com>
2018-01-13 12:22:30 -0800

    documentation

--- verilog/pwm/pwm.v
+++ verilog/pwm/pwm.v
@@ -1,12 +1,13 @@
 /*
- * demonstrate PWM with Verilog
+ * Demonstrate PWM with Verilog
+ *
  */
 module pwm(clock, dips, led);
-    input wire clock;
-    input wire dips[3:0];
-    output wire led;
-    reg [15:0] count;
-    reg [15:0] duty;
+    input wire clock;       /* 50 MHz clock */
+    input wire dips[3:0];   /* four slide switches */
+    output wire led;        /* output LED */
+    reg [15:0] count;       /* counter register to increment on each clock */
+    reg [15:0] duty;        /* when count > duty, LED is on */
 
     initial begin
         count <= 16'b0;
@@ -14,9 +15,12 @@ module pwm(clock, dips, led);
     end
 
     /* 
-     * 50 MHz clock = 20 ns per clock 
-     * 8-bit register = 5.12 us between overflows (195.3 kHz)
-     * 16-bit register = 1.31 ms between overflows (762.9 Hz)
+     * Reminder: DE10-Nano's 50 MHz clock = 20 ns per clock 
+     *
+     *    8-bit register = 5.12 us between overflows (195.3 kHz)
+     *   16-bit register = 1.31 ms between overflows (762.9 Hz)
+     *
+     * 762.9 Hz switching time seems to work well for a standard LED
      */
     always @(posedge clock) begin
         count = count + 1'b1;
@@ -28,6 +32,14 @@ module pwm(clock, dips, led);
 
     /*
      * four dip switches control the most significant bits of duty
+     *   0000 = 100.0% duty cycle
+     *   0001 = 92.75% duty cycle
+     *   0010 = 87.50% duty cycle
+     *   0100 = 75.00% duty cycle
+     *   1000 = 50.00% duty cycle
+     *   1100 = 25.00% duty cycle
+     *   1110 = 12.50% duty cycle
+     *   1111 =  6.25% duty cycle
      */
     assign duty[15] = dips[3];
     assign duty[14] = dips[2];
