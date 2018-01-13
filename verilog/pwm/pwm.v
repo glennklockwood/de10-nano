4631d2e1cdd474a200d7b0c82746b6b62f2d9593
Glenn K. Lockwood <glennklockwood@gmail.com>
2018-01-13 09:19:10 -0800

    add baseline LED blink

--- verilog/pwm/pwm.v
+++ verilog/pwm/pwm.v
@@ -1,3 +1,19 @@
 /*
  * demonstrate PWM with Verilog
  */
+
+module pwm(clock, LED);
+    input wire clock;
+    output wire LED;
+    reg [31:0] count;
+
+    initial begin
+        count <= 32'b0;
+    end
+
+    always @(posedge clock) begin
+        count <= count + 1'b1;
+    end
+
+    assign LED = count[24];
+endmodule
